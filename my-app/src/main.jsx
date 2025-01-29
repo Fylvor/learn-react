46db563b2774f03f12f271cbdaf22b9662140036
Fylvor <achrafatif01@gmail.com>
2025-01-29 14:00:54 +0100

    first react challenge

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,6 +1,9 @@
-import {createRoot} from "react-dom/client"
-
+import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"))
 
-root.render(<h1>hello, world</h1>)
+root.render(<ul>
+  <li>React is a highly marketable skill.</li>
+  <li>Many job listings specifying it as a required skill.</li>
+  <li>Developers proficient in React can negotiate higher salaries.</li>
+</ul>)
